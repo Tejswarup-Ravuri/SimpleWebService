783f62adebbd46dec471ab09383ab8ce7d9b84dd
Ravuri <travuri@teksystems.com>
2026-01-04 19:39:34 +0530

    Get and post operations in spring boot

--- src/main/java/com/learnings/SimpleWebService/controller/ProductController.java
+++ src/main/java/com/learnings/SimpleWebService/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.learnings.SimpleWebService.controller;
 import com.learnings.SimpleWebService.model.Product;
 import com.learnings.SimpleWebService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -17,4 +16,16 @@ public class ProductController {
     {
         return productservice.getProducts();
     }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return productservice.getProductByProdId(prodId);
+    }
+
+    @PostMapping("/products")
+    public String addProduct( @RequestBody Product product){
+        productservice.addProduct(product);
+        return "Product added successfully";
+    }
+
 }
--- src/main/java/com/learnings/SimpleWebService/service/ProductService.java
+++ src/main/java/com/learnings/SimpleWebService/service/ProductService.java
@@ -3,16 +3,25 @@ package com.learnings.SimpleWebService.service;
 import com.learnings.SimpleWebService.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
-    List<Product> products= Arrays.asList(new Product(101,"IQOO",30000),
-            new Product(201,"Iphone",50000));
+    List<Product> products= new ArrayList<>(Arrays.asList(new Product(101,"IQOO",30000),
+            new Product(201,"Iphone",50000)));
     public List<Product> getProducts(){
 
         return products;
+    }
+
+    public Product getProductByProdId(int prodId){
+        return products.stream().filter(p->p.getProductId()==prodId).findFirst().get();
+    }
+
+    public void addProduct(Product productFromInput){
+        products.add(productFromInput);
     }
 
 }
